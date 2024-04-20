9ab4b15f23766b87867bc93f5934b1316688e34e
Vulpolox <113536413+Vulpolox@users.noreply.github.com>
2024-04-20 05:42:24 -0500

    Finished everything but csv writing

--- PhoneBookManagement/src/Controllers/EditContactController.java
+++ PhoneBookManagement/src/Controllers/EditContactController.java
@@ -1,8 +1,11 @@
 package Controllers;
 
+import Modules.Contact;
+import Modules.UserDataAccess;
 import View.ContactsView;
 import View.EditContactView;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -31,19 +34,48 @@ public class EditContactController
 
         editContactView.addRemoveButtonListener(new ActionListener()
         {
-            // code for cancelButton press
+            // code for remove button press
             public void actionPerformed(ActionEvent e)
             {
+                UserDataAccess uda = new UserDataAccess();
+                boolean success = uda.removeContact(editContactView.getUser(), editContactView.getContactToEdit());
 
+                // give the user feedback whether the contact was successfully removed
+                if (success)
+                    JOptionPane.showMessageDialog(null, "Contact removed from phonebook");
+                else
+                    JOptionPane.showMessageDialog(null, "Failed to remove contact");
+
+                // set the addContactView to be invisible
+                editContactView.setVisible(false);
+
+                // re-enable the contacts view
+                ContactsView cv = new ContactsView(editContactView.getUser());
+                ContactsController contactController = new ContactsController(cv);
+                cv.setVisible(true);
             }
         });
 
         editContactView.addEditButtonListener(new ActionListener()
         {
-            // code for cancelButton press
+            // code for edit button press
             public void actionPerformed(ActionEvent e)
             {
+                // initialize new contact
+                Contact newContact = new Contact(editContactView.getNewFirstName(),
+                                                 editContactView.getNewLastName(),
+                                                 editContactView.getNewPhoneNumber());
 
+                // remove the old contact and add the new contact
+                UserDataAccess uda = new UserDataAccess();
+                uda.removeContact(editContactView.getUser(), editContactView.getContactToEdit());
+                boolean success = uda.addContact(editContactView.getUser(), newContact);
+
+                // give the user feedback whether they successfully updated the contact
+                if (success)
+                    JOptionPane.showMessageDialog(null, "Updated contact");
+                else
+                    JOptionPane.showMessageDialog(null, "Failed to update contact");
             }
         });
     }
